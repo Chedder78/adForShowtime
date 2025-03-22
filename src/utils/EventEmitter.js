497e731bf01d74290cd0a892e359c5927ba8569c
chad morrison <134284764+Chedder78@users.noreply.github.com>
2025-03-22 11:22:39 -0700

    Create EventEmitter.js

---
+++ src/utils/EventEmitter.js
@@ -0,0 +1,19 @@
+// /src/utils/EventEmitter.js
+// Utility: Basic event system to decouple modules
+
+export default class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    on(event, listener) {
+        if (!this.events[event]) this.events[event] = [];
+        this.events[event].push(listener);
+    }
+
+    emit(event, data) {
+        if (this.events[event]) {
+            this.events[event].forEach(listener => listener(data));
+        }
+    }
+}
