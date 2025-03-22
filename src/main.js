c973f592332c1d9fe79427f3dfe96db1acba5384
chad morrison <134284764+Chedder78@users.noreply.github.com>
2025-03-22 11:20:19 -0700

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// /src/main.js
+// Entry point: Initializes and starts the game
+
+import GameManager from './managers/GameManager.js';
+
+window.onload = () => {
+    const canvas = document.getElementById('gameCanvas');
+    const ctx = canvas.getContext('2d');
+
+    const game = new GameManager(canvas, ctx);
+    game.start();
+};
