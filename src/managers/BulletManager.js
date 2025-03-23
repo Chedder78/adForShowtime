659efb6cb6dc518e767f6f5571cc35ff2b6e2e0a
chad morrison <134284764+Chedder78@users.noreply.github.com>
2025-03-22 22:04:25 -0700

    Update BulletManager.js

--- src/managers/BulletManager.js
+++ src/managers/BulletManager.js
@@ -1,7 +1,5 @@
 // /src/managers/BulletManager.js
-// Handles all bullets (creation + update + cleanup)
-
-import Bullet from './src/entities/Bullet.js';
+import Bullet from '../entities/Bullet.js';
 
 export default class BulletManager {
     constructor() {
@@ -9,23 +7,20 @@ export default class BulletManager {
         this.cooldown = 0;
     }
 
-    // inside BulletManager.js fire()
-fire(player) {
-    if (this.cooldown <= 0) {
-        if (player.doubleShotMode) {
-            this.bullets.push(new Bullet(player.x - 5, player.y, player.angle));
-            this.bullets.push(new Bullet(player.x + 5, player.y, player.angle));
-        } else {
-            this.bullets.push(new Bullet(player.x, player.y, player.angle));
+    fire(player) {
+        if (this.cooldown <= 0) {
+            if (player.doubleShotMode) {
+                this.bullets.push(new Bullet(player.x - 5, player.y, player.angle));
+                this.bullets.push(new Bullet(player.x + 5, player.y, player.angle));
+            } else {
+                this.bullets.push(new Bullet(player.x, player.y, player.angle));
+            }
+            this.cooldown = 300;
         }
-        this.cooldown = 300;
     }
-}
-
 
     update() {
-        this.cooldown -= 16; // Approx 60fps delta compensation
-
+        this.cooldown -= 16;
         this.bullets.forEach(b => b.update());
         this.bullets = this.bullets.filter(b => b.active);
     }
